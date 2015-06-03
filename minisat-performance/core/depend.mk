178ce952822c562e5b1f2b9e2164243dba0a34f6
Ed Zulkoski <ezulkosk@gsd.uwaterloo.ca>
2015-06-03 16:48:51 -0400

    initial graph output

---
+++ minisat-performance/core/depend.mk
@@ -0,0 +1,27 @@
+/home/ezulkosk/git/vsids/minisat-performance/core/Main.o /home/ezulkosk/git/vsids/minisat-performance/core/Main.or /home/ezulkosk/git/vsids/minisat-performance/core/Main.od /home/ezulkosk/git/vsids/minisat-performance/core/Main.op: /home/ezulkosk/git/vsids/minisat-performance/core/Main.cc \
+ /home/ezulkosk/git/vsids/minisat-performance/utils/System.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/IntTypes.h \
+ /home/ezulkosk/git/vsids/minisat-performance/utils/ParseUtils.h \
+ /home/ezulkosk/git/vsids/minisat-performance/utils/Options.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Vec.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/XAlloc.h \
+ /home/ezulkosk/git/vsids/minisat-performance/core/Dimacs.h \
+ /home/ezulkosk/git/vsids/minisat-performance/core/SolverTypes.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Alg.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Map.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Alloc.h \
+ /home/ezulkosk/git/vsids/minisat-performance/core/Solver.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Heap.h
+/home/ezulkosk/git/vsids/minisat-performance/core/Solver.o /home/ezulkosk/git/vsids/minisat-performance/core/Solver.or /home/ezulkosk/git/vsids/minisat-performance/core/Solver.od /home/ezulkosk/git/vsids/minisat-performance/core/Solver.op: /home/ezulkosk/git/vsids/minisat-performance/core/Solver.cc \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Sort.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Vec.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/IntTypes.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/XAlloc.h \
+ /home/ezulkosk/git/vsids/minisat-performance/core/Solver.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Heap.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Alg.h \
+ /home/ezulkosk/git/vsids/minisat-performance/utils/Options.h \
+ /home/ezulkosk/git/vsids/minisat-performance/utils/ParseUtils.h \
+ /home/ezulkosk/git/vsids/minisat-performance/core/SolverTypes.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Map.h \
+ /home/ezulkosk/git/vsids/minisat-performance/mtl/Alloc.h
